e9590c9bc19084abffc4e39799271fb24c77b1b9
wachira <wachirastudeo@gmail.com>
2025-01-23 06:52:48 +0700

    create models

---
+++ go-sample-api/internal/models/Movie.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Movie struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	ReleaseDate time.Time `json:"release_date"`
+	Runtime     int       `json:"runtime"`
+	MPAARating  string    `json:"mpaa_rating"`
+	Description string    `json:"description"`
+	Image       string    `json:"image"`
+	Created_at  time.Time `json:"-"`
+	Updated_at  time.Time `json:"- "`
+}
