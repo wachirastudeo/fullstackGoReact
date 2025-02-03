f48222387dba2c91de196e4d130a4e6d86e4bd60
wachira <wachirastudeo@gmail.com>
2025-02-03 13:21:33 +0700

    all genres

--- go-sample-api/internal/repository/dbrepo/postgres_dbrepo.go
+++ go-sample-api/internal/repository/dbrepo/postgres_dbrepo.go
@@ -20,6 +20,60 @@ func (m *PostgresDBRepo) Connection() *sql.DB {
 	return m.DB
 }
 
+// สร้างฟังชั่นค้นหา user จาก email
+
+func (m *PostgresDBRepo) GetUserByEmail(email string) (*models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+	query := `select id,email,password,first_name,last_name,created_at,updated_at from users where email = $1`
+	// prepare statement sql injection
+
+	var user models.User
+	row := m.DB.QueryRowContext(ctx, query, email)
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.FirstName,
+		&user.LastName,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+
+}
+
+// สร้าง Function ค้นหา User จาก ID
+func (m *PostgresDBRepo) GetUserByID(id int) (*models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password,
+			created_at, updated_at from users where id = $1`
+
+	var user models.User
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
 func (m *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
 	//context handle query time out
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
@@ -207,56 +261,35 @@ func (m *PostgresDBRepo) OneMovieForEdit(id int) (*models.Movie, []*models.Genre
 	return &movie, allGenres, err
 }
 
-// สร้างฟังชั่นค้นหา user จาก email
-
-func (m *PostgresDBRepo) GetUserByEmail(email string) (*models.User, error) {
+// all Genres
+func (m *PostgresDBRepo) AllGenres() ([]*models.Genre, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
-	query := `select id,email,password,first_name,last_name,created_at,updated_at from users where email = $1`
-	// prepare statement sql injection
-
-	var user models.User
-	row := m.DB.QueryRowContext(ctx, query, email)
-	err := row.Scan(
-		&user.ID,
-		&user.Email,
-		&user.Password,
-		&user.FirstName,
-		&user.LastName,
-		&user.CreatedAt,
-		&user.UpdatedAt,
-	)
+	query := `select id, genre,created_at,updated_at from genres order by genre`
+	rows, err := m.DB.QueryContext(ctx, query)
 	if err != nil {
 		return nil, err
 	}
-	return &user, nil
+	defer rows.Close()
+	var genres []*models.Genre
+	for rows.Next() {
+		g := &models.Genre{}
+		err := rows.Scan(
+			&g.ID,
+			&g.Genre,
+			&g.CreatedAt,
+			&g.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		genres = append(genres, g)
+	}
+	return genres, nil
 
 }
 
-// สร้าง Function ค้นหา User จาก ID
-func (m *PostgresDBRepo) GetUserByID(id int) (*models.User, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
-	defer cancel()
+// insert movie
+func (m *PostgresDBRepo) InsertMovie(movie *models.Movie) {
 
-	query := `select id, email, first_name, last_name, password,
-			created_at, updated_at from users where id = $1`
-
-	var user models.User
-	row := m.DB.QueryRowContext(ctx, query, id)
-
-	err := row.Scan(
-		&user.ID,
-		&user.Email,
-		&user.FirstName,
-		&user.LastName,
-		&user.Password,
-		&user.CreatedAt,
-		&user.UpdatedAt,
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &user, nil
 }
