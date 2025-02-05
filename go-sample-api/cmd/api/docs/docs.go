8004820dc0f1bea9ffb4700920ac176f97f0ac33
wachira <wachirastudeo@gmail.com>
2025-02-05 08:00:39 +0700

    docs swagger

--- go-sample-api/cmd/api/docs/docs.go
+++ go-sample-api/cmd/api/docs/docs.go
@@ -522,7 +522,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "localhost:7070",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Movies API with GO and PostgreSQL",
